4b1337da5fc26b160589b2300a4af973c0688f26
Titouan Renard <titouan.renard@gmail.com>
2022-03-25 11:26:18 +0100

    ray LTC skeletton

--- LTCRL/models.py
+++ LTCRL/models.py
@@ -64,3 +64,68 @@ class NaiveRNN(TorchRNN, nn.Module):
         lh = list(h.squeeze(0).swapaxes(0,1))
         
         return action_out, lh
+
+
+
+class LTC(TorchRNN, nn.Module):
+    def __init__(self,
+                 obs_space,
+                 action_space,
+                 num_outputs,
+                 model_config,
+                 name):
+        
+        nn.Module.__init__(self)
+        super().__init__(obs_space, action_space, num_outputs, model_config, name)
+
+        # TODO : process the relevant parameters here
+        self.elapsed_time = 0.2 #kind of a random value, think about this 
+        
+        # TODO : declare the nn functional stuff here
+    
+    
+    # TODO : pytorch sigmoid implementation 
+    def _sigmoid(self, v_pre, mu, sigma):
+        pass
+
+    # TODO : FUSED step ODE solver
+    def _ode_solver(self, inputs, state, elapsed_time):
+        pass
+
+    # TODO : input mapping function
+    def _map_inputs(self, inputs):
+        pass
+    
+    # TODO : output mapping function
+    def _map_outputs(self, state):
+        
+        pass
+        action = 0
+        value = 0
+        return action, value
+
+    # TODO implement
+    @override(ModelV2)
+    def get_initial_state(self):
+        pass
+
+    # TODO think of a relevant value function
+    @override(ModelV2)
+    def value_function(self):
+        pass
+
+    # TODO implement the LTC forward pass
+    @override(TorchRNN)
+    def forward_rnn(self, inputs, state, seq_lens):
+        """
+            Forward pass throught the RNN
+        """
+        
+        inputs = self._map_inputs(inputs) # input mapping
+        next_state = self._ode_solver(inputs, state, self.elapsed_time) # compute the ODE step
+        action, _ = self._map_outputs(next_state) # compute the ODE step
+        
+        
+        
+        return outputs, next_state
+
