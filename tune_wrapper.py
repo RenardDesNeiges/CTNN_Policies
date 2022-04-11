2a674b1e2f844ffba25b74c3508f43c34f4e7257
Titouan Renard <titouan.renard@gmail.com>
2022-04-11 11:22:43 +0200

    better hyper_parameters

--- tune_wrapper.py
+++ tune_wrapper.py
@@ -8,21 +8,48 @@ import LTCRL.models as models
 ModelCatalog.register_custom_model("RNN", models.NaiveRNN)
 ModelCatalog.register_custom_model("LTC", models.LTC)
 
+NUM_CPUS = 8 # changed based on PC / cluster
+num_samples_each_worker = int(16384 / NUM_CPUS)
+
 
 model = {
-        "custom_model": "RNN",
-        # "custom_model_config": 
-        #     {"sample_frequency": 9,
-        #     "state_size": 12,
-        #     "ode_unfolds": 5,
-        #     "epsilon": 1e-8,
-        #     },
+    
+        "custom_model": "LTC",
+        "custom_model_config": 
+            {"sample_frequency": 5,
+            "state_size": 8,
+            "ode_unfolds": 3,
+            "epsilon": 1e-8,
+            },
         }
 
 # environment config
 config = {
-    "env": "Cartpole-v1", # Environment
-    "num_workers": 4, # number of worker envs
+    "num_envs_per_worker": 1, 
+    "use_critic": True,
+    # If true, use the Generalized Advantage Estimator (GAE)
+    # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
+    "use_gae": True,
+    "lr": 1e-4,
+    "env": "CartPole-v1", # Environment
+    "num_workers": NUM_CPUS, # number of worker envs
+    "train_batch_size": NUM_CPUS*num_samples_each_worker, 
+    "num_sgd_iter": 10,
+    "rollout_fragment_length": num_samples_each_worker,
+    
+    "vf_loss_coeff": 0.5,
+    "lambda":0.95, 
+    "grad_clip": 0.5, 
+    #"use_kl_loss": False,
+    # "kl_coeff": 0.0, 
+    # "kl_target": 0.01,
+    "entropy_coeff": 0.0,
+    "observation_filter": "MeanStdFilter", # normalize observation space (very important)
+    "clip_actions": True,
+    "num_sgd_iter": 10,
+    "clip_param": 0.2,
+    "vf_clip_param": 40, # may need to increase depending on your reward scaling
+    "sgd_minibatch_size": 256,
     "framework": "torch", # we run pytorch, not tensorflow
     "model": model,
     "evaluation_interval": 20,
@@ -37,3 +64,5 @@ config = {
 ray.init(local_mode=False)
 # Instanciate the PPO trainer object
 tune.run(PPOTrainer, config=config)
+
+
